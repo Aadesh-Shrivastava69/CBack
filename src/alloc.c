6ebbb683d97636919339a2266be7fa244d35f287
skyveil <aaryan.omdwivedi@gmail.com>
2026-01-30 16:35:52 +0530

    Added basic allocator

--- src/alloc.c
+++ src/alloc.c
@@ -20,3 +20,28 @@ arena_t arena_create(u32 size) {
 
     return a;
 }
+
+static inline u32 align_up(u32 size, u32 val) {
+    return (size + (val - 1)) & ~(val - 1);
+}
+
+void *arena_alloc(arena_t *a, u32 size) {
+    size = align_up(size, 8);
+
+    if (a->used + size > a->size)
+        return NULL;
+
+    void *ptr = a->base + a->used;
+    a->used += size;
+    return ptr;
+}
+
+void arena_reset(arena_t *a) {
+    a->used = 0;
+}
+
+void arena_destroy(arena_t *a) {
+    munmap(a->base, a->size);
+    a->base = NULL;
+    a->size = a->used = 0;
+}
--- tests/alloc_test.c
+++ tests/alloc_test.c
@@ -1,5 +1,24 @@
 #include "alloc.h"
 
+#include <stdio.h>
+
 int main() {
-    arena_t arena = arena_create(100);
+    arena_t arena = arena_create(40000);
+
+    int *ptr = arena_alloc(&arena, 10);
+    ptr[0] = 2020;
+    ptr[1] = 2021;
+
+    printf("%d, %d\n", ptr[0], ptr[1]);
+
+    arena_reset(&arena);
+    int *ptr2 = arena_alloc(&arena, 20);
+    ptr[0] = 2023;
+    ptr[1] = 2024;
+
+    printf("%d, %d\n", ptr[0], ptr[1]);
+
+    arena_destroy(&arena);
+
+    // printf("%d, %d\n", ptr[0], ptr[1]); // segfault
 }
